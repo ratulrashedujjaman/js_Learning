794efa19370e5157caba7e8c867eb8f3e6aa48a3
rashedujjaman <ratulrashedujjaman@gmail.com>
2024-12-18 19:29:33 +0600

    Part23&24

---
+++ Part_23/app.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * JavaScript Strict Mode
+ * 
+ *
+ */
+'use strict';
+let x=5;
+console.log(x);
+
+name(5,4);
+function name(p1,p2){
+    'use strict';
+    let y =10;
+    console.log(p1,p2);
+}
+
+// let private ='Some text';
+// let public ='Some text';
+// let package ='Some text';
