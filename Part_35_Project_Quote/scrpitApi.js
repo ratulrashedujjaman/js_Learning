01a029b15965c94a48d3040d2a9e50b251d86b5f
rashedujjaman <ratulrashedujjaman@gmail.com>
2025-01-15 21:20:15 +0600

    35,36

---
+++ Part_35_Project_Quote/scrpitApi.js
@@ -0,0 +1,27 @@
+const quoteElement = document.querySelector('#quote');
+const authorElement = document.querySelector('#author');
+const button = document.querySelector('#button');
+
+const urlAPI =  'https://api.api-ninjas.com/v1/quotes';
+const keyAPI = 'poL3smpK8OSCT1Yy+KyhUg==sONkPTm0wcsFhL4a';
+
+const getQuote = () => {
+    fetch(urlAPI, {
+        headers: { 'X-Api-Key': keyAPI,
+
+        },
+    })
+    .then(data => data.json())
+    .then(item => {
+        // console.log(item[0]);
+        quoteElement.textContent = item[0].quote;
+        authorElement.textContent = item[0].author;
+        
+    })
+    .catch(error => {
+        console.error('Error Fatching Quotes' , error);
+    });
+    
+};
+getQuote();
+button.addEventListener('click' , getQuote);
